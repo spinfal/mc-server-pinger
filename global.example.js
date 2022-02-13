cdfd04a699935511ccb9b11ec93a0cbccbd13cc1
spin~ <spin@seized.email>
2022-02-12 16:59:03 -0800

    I'm kinda dumb

--- global.example.js
+++ global.example.js
@@ -5,5 +5,6 @@ module.exports = {
     serverIP: "YOUR SERVER IP",
     serverPort: 25565, // Default port for Minecraft servers
     pingEveryone: true, // Pings everyone when the server is down
-    message: "The server is down!"
+    message: "The server is down!",
+    interval: 1000 * 60 * 5 // every 5 minutes
 }
