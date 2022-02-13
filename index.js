7de455778b69cc8a69610b2155ea09c14f8ba739
spin~ <spin@seized.email>
2022-02-12 16:58:22 -0800

    Add interval

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const util = require('minecraft-server-util');
 const Global = require('./global.js');
 const webhook = new Webhook(Global.webhook);
 
-util.status(Global.serverIP, { port: Global.serverPort ?? 25565 })
-    .then(res => {
-        if (res.version === null) {
-            const embed = new MessageBuilder()
-                .setText(Global.pingEveryone ? `@everyone ${Global.message}` : Global.message)
-                .setColor('#ff0000')
-                .setThumbnail('https://cdn.spin.rip/content/2022/02/02/1643794001828-mc-block.png')
-                .setDescription(`The server ${Global.serverIP} is down!`)
-                .setFooter('Made by Spinfal', 'https://cdn.discordapp.com/emojis/727618803772555325.webp?size=44&quality=lossless')
-                .setTimestamp();
+setInterval(() => {
+    util.status(Global.serverIP, { port: Global.serverPort ?? 25565 })
+        .then(res => {
+            if (res.version === null) {
+                const embed = new MessageBuilder()
+                    .setText(Global.pingEveryone ? `@everyone ${Global.message}` : Global.message)
+                    .setColor('#ff0000')
+                    .setThumbnail('https://cdn.spin.rip/content/2022/02/02/1643794001828-mc-block.png')
+                    .setDescription(`The server ${Global.serverIP} is down!`)
+                    .setFooter('Made by Spinfal', 'https://cdn.discordapp.com/emojis/727618803772555325.webp?size=44&quality=lossless')
+                    .setTimestamp();
 
-            // /^{0-9}[18]$/.test(Global.roleID) ? `<@{${Global.roleID}}> ${Global.message}` : Global.message
-            webhook.send(embed);
-        }
-    }).catch(err => {
-        console.log(err);
-    });
+                // /^{0-9}[18]$/.test(Global.roleID) ? `<@{${Global.roleID}}> ${Global.message}` : Global.message
+                webhook.send(embed);
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+}, Global.interval);
