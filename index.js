b658618294f5b32a97f76e3f418ad1c63626a03f
spin~ <65109978+spinfal@users.noreply.github.com>
2022-02-14 21:01:25 +0000

    Fix

--- index.js
+++ index.js
@@ -15,19 +15,18 @@ webhook.info('**MC Down Detector**', 'Webhook setup succesfully', `The server wi
 setInterval(() => {
     util.status(Global.serverIP, { port: Global.serverPort })
         .then(res => {
-            if (res.version === null) {
-                const embed = new MessageBuilder()
-                    .setText(Global.pingEveryone ? `@everyone ${Global.message}` : Global.message)
-                    .setColor('#ff0000')
-                    .setThumbnail('https://cdn.spin.rip/content/2022/02/02/1643794001828-mc-block.png')
-                    .setDescription(`The server ${Global.serverIP} is down!`)
-                    .setFooter('Made by Spinfal', 'https://cdn.discordapp.com/emojis/727618803772555325.webp?size=44&quality=lossless')
-                    .setTimestamp();
-
-                // /^{0-9}[18]$/.test(Global.roleID) ? `<@{${Global.roleID}}> ${Global.message}` : Global.message
-                webhook.send(embed);
-            }
+            console.log('Server is up!')
         }).catch(err => {
+            const embed = new MessageBuilder()
+                .setText(Global.pingEveryone ? `@everyone ${Global.message}` : Global.message)
+                .setColor('#ff0000')
+                .setThumbnail('https://cdn.spin.rip/content/2022/02/02/1643794001828-mc-block.png')
+                .setDescription(`The server ${Global.serverIP} is down!`)
+                .setFooter('Made by Spinfal', 'https://cdn.discordapp.com/emojis/727618803772555325.webp?size=44&quality=lossless')
+                .setTimestamp();
+
+            // /^{0-9}[18]$/.test(Global.roleID) ? `<@{${Global.roleID}}> ${Global.message}` : Global.message
+            webhook.send(embed);
             console.log(err);
         });
 }, Global.interval);
